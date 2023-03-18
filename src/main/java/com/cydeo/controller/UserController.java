95d5b57b87f7231a9b714c685040baef6d5c5f8d
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-03-18 10:51:00 -0500

    Validation Started

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,7 +5,10 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -29,7 +32,14 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public String insertUser(@ModelAttribute("user") UserDTO user) {
+    public String insertUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("roles", roleService.findAll());
+            model.addAttribute("users", userService.findAll());
+            return "/user/create";
+        }
+
         userService.save(user);
         return "redirect:/user/create";
     }
@@ -45,7 +55,7 @@ public class UserController {
     }
 
     @PostMapping("/update")
-    public String updateUser(@ModelAttribute("user") UserDTO user){
+    public String updateUser(@Valid @ModelAttribute("user") UserDTO user){
         userService.update(user);
         return "redirect:/user/create";
     }
